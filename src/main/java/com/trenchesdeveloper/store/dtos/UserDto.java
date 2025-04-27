5117bf950f469ceb119f032470d4dcd3d7f7043f
trenchesdeveloper <opeyemisamuel222@gmail.com>
2025-04-27 22:07:55 +0100

    addd get user by id endpoint

---
+++ src/main/java/com/trenchesdeveloper/store/dtos/UserDto.java
@@ -0,0 +1,14 @@
+package com.trenchesdeveloper.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UserDto {
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
--- src/main/java/com/trenchesdeveloper/store/repositories/UserRepository.java
+++ src/main/java/com/trenchesdeveloper/store/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.trenchesdeveloper.store.repositories;
 
 import com.trenchesdeveloper.store.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
